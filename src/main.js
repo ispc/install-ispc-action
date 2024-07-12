49583aaab24cd1a44bfc2f342af2e0e67002f62c
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-07-12 19:37:54 +0100

    Add fallback for version detection logic
    It may happen that runner is banned by API rate limits. In this case,
    just clone git repository and fetch the latest tag to define the latest
    version.

--- src/main.js
+++ src/main.js
@@ -4,19 +4,42 @@ import { resolve } from "path";
 import { downloadTool, extractTar, extractZip } from '@actions/tool-cache';
 import { addPath, setFailed, getInput } from '@actions/core';
 import { platform as osPlatform, arch as osArch } from 'node:os';
+import { existsSync, rmSync } from 'node:fs';
+import { execSync } from 'node:child_process';
 
 
 async function getLatestVersion() {
     // Note: The GitHub API is rate limited to 60 requests per hour for unauthenticated users.
     // I am not quite sure that it is reliable to query the latest version from
     // the GitHub API because of the rate limits.
+    let version;
     let response = await fetch(`https://api.github.com/repos/ispc/ispc/releases/latest`);
     if(response.status !== 200) {
         let errorDetails = await response.text();
-        throw new Error (`Unable to query latest version: ${response.statusText} - ${errorDetails}`);
+        console.log(`Unable to query latest version: ${response.statusText} - ${errorDetails}`);
+
+        try {
+            // Fallback to local git repository
+            const repoPath = 'ispc-repo';
+            if (!existsSync(repoPath)) {
+                execSync(`git clone --depth=1 https://github.com/ispc/ispc.git ${repoPath}`);
+            }
+
+            console.log('Fetching latest tags from the repository...');
+            execSync(`git -C ${repoPath} fetch --tags`);
+
+            let latestTagSHA = execSync(`git -C ${repoPath} rev-list --tags --max-count=1`).toString().trim();
+            version = execSync(`git -C ${repoPath} describe --tags ${latestTagSHA}`).toString().trim();
+
+            rmSync(repoPath, { recursive: true, force: true });
+        } catch (gitError) {
+            throw new Error(`Unable to get latest version from git repository: ${gitError.message}`);
+        }
+    } else {
+        let body = await response.json()
+        version = body.tag_name;
     }
-    let body = await response.json();
-    let version = body.tag_name;
+
     if(version.charAt(0) === 'v') {
         version = version.substring(1);
     }
@@ -136,8 +159,8 @@ async function getIspc(version, platform, architecture) {
     let archPrefix = architecture === 'oneapi' ? '-' : '.';
     let archStr = architecture ? `${archPrefix}${architecture}` : "";
     let extension = platform === "windows" ? ".zip" : ".tar.gz";
-    let archive_name = `ispc-${version}-${platform}${archStr}`;
-    let url = `https://github.com/ispc/ispc/releases/download/${version}/${archive_name}${extension}`;
+    let archiveName = `ispc-${version}-${platform}${archStr}`;
+    let url = `https://github.com/ispc/ispc/releases/download/${version}/${archiveName}${extension}`;
     let dir = `ispc-releases`;
     console.log(`Downloading ISPC archive ${url}`);
     const archive = await downloadTool(url);
@@ -146,7 +169,7 @@ async function getIspc(version, platform, architecture) {
     } else {
         await extractTar(archive, dir);
     }
-    let binDir = resolve(dir, archive_name, "bin");
+    let binDir = resolve(dir, archiveName, "bin");
     return binDir;
 }
 
